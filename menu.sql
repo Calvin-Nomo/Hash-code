e25c67186d37912acf19d8fb6150dda45c2f07d0
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-22 12:45:04 +0100

    Update menu.sql
    Added the note table for the client

--- menu.sql
+++ menu.sql
@@ -5,3 +5,9 @@ Order_date datetime,
 Order_type enum("Dine In", "Take Away"), 
 No_Table int 
 ); 
+Create table Note(
+Note_ID int AUTO_INCREMENT primary key, 
+Order_ID int, 
+Note VARCHAR(255),
+Foriegn key(Order_ID) References Order(Order_ID ) 
+);
