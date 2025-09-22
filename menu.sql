746288f8a90891a71564f1f65b6c0fc0d90ee2d1
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-22 12:37:48 +0100

    Update menu.sql
    Completed the order table

--- menu.sql
+++ menu.sql
@@ -1,6 +1,7 @@
+Create database Menu;
 Create table Order(
-Order_ID int AUTO_INCREMENT primary key
-
-
-
+Order_ID int AUTO_INCREMENT primary key, 
+Order_date datetime, 
+Order_type enum("Dine In", "Take Away"), 
+No_Table int 
 ); 
