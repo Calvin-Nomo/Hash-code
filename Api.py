f693f067ab552e1a9f406553159158a680015c5a
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-22 18:57:12 +0100

    Update Api.py
    Created a get method for greetings

--- Api.py
+++ Api.py
@@ -1,1 +1,11 @@
 # welcome to my api
+from fastapi import FASTAPI
+from pydantic import Base model 
+
+app=FASTAPI() 
+
+@app.get(/)
+def greetings():
+    return {
+     "Message ":"Hello World" 
+}
