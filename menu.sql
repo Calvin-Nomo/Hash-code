af9876f27ed24d8cd2db9ed6ec525a24389170cc
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-22 13:07:11 +0100

    Update menu.sql
    Added the Product and Order_Items table

--- menu.sql
+++ menu.sql
@@ -11,3 +11,18 @@ Order_ID int,
 Note VARCHAR(255),
 Foriegn key(Order_ID) References Order(Order_ID ) 
 );
+Create table Product(
+No_Product int AUTO_INCREMENT primary key, 
+Product_Name VARCHAR(255), 
+Product_Description VARCHAR(255), 
+Unit_Price float, 
+Image_link VARCHAR(255) 
+);
+Create table Order_Items(
+Item_ID int AUTO_INCREMENT primary key, 
+Order_ID int, 
+No_Product int, 
+Quantity int, 
+Foriegn key (No_Product) References Product(No_Produit), 
+Foriegn key (Order_ID ) References Order (Order_ID) 
+); 
