75db1513fdc6eae29d5ca159de36db362e20cbc7
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-27 10:42:24 +0100

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,6 +1,14 @@
 # welcome to my api
 from fastapi import FASTAPI
 from pydantic import Base model 
+import pymysql
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="Bineli26",
+   database="Order_System", cursorclass=pymysql.cursors.DictCursor  # so results come as dicts instead of tuples
+)
 
 app=FASTAPI() 
 
