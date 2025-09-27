18f3ef6a6968fe0323cfcfeeba0ba716458b0a21
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-27 10:59:13 +0100

    Update Api.py

--- Api.py
+++ Api.py
@@ -3,17 +3,25 @@ from fastapi import FASTAPI
 from pydantic import Base model 
 import pymysql
 
-connection = pymysql.connect(
+DB= pymysql.connect(
     host="localhost",
     user="root",
     password="Bineli26",
    database="Order_System", cursorclass=pymysql.cursors.DictCursor  # so results come as dicts instead of tuples
 )
 
-app=FASTAPI() 
+cursor=DB.connection()
 
+app=FASTAPI() 
+########Here is all the Get Method#######
 @app.get(/)
 def greetings():
     return {
      "Message ":"Hello World" 
 }
+
+@app.get(/Product)
+def get_product():
+ sql_command="Select* from Product" cursor.execute(sql_command)
+ product=cursor.fetchall
+    return product 
