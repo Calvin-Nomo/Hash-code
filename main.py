588aff8975dcd7991483f6d8b96056f84442fc52
Calvin-Nomo <calvinnomo007@gmail.com>
2025-09-29 10:50:25 +0100

    removed all the mistakes

---
+++ main.py
@@ -0,0 +1,75 @@
+# welcome to my api
+from fastapi import FASTAPI
+from pydantic import BaseModel 
+import pymysql
+
+DB= pymysql.connect(
+    host="localhost",
+    user="root",
+    password="Bineli26",
+   database="Order_System", cursorclass=pymysql.cursors.DictCursor  # so results come as dicts instead of tuples
+)
+
+cursor=DB.connection()
+
+app=FASTAPI() 
+######## Get Method#######
+@app.get('/')
+def greetings():
+    return {
+     "Message ":"Hello World" 
+}
+
+@app.get('/Product')
+def get_product():
+    sql_command="Select* from Product"
+    cursor.execute(sql_command)
+    product=cursor.fetchall
+    return product
+
+@app.get('/Order')
+def get_order():
+    sql_command="Select* from Order " 
+    cursor.execute(sql_command)
+    order =cursor.fetchall
+    return order
+@app.get('/Order_Items')
+def get_orderitems():
+    sql_command="Select* from Order_Items " 
+    cursor.execute(sql_command)
+    Items=cursor.fetchall
+    return Items
+
+@app.get('/Payment' )
+def get_payment():
+    sql_command="Select* from Payment" 
+    cursor.execute(sql_command)
+    payments=cursor.fetchall
+    return payments
+
+
+@app.get('/Reservation')
+def get_reservation():
+    sql_command="Select * from Reservation "
+    cursor.execute(sql_command)
+    reservations =cursor.fetchall
+    return reservations
+
+@app.get('/Client')
+def get_client():
+    sql_command="Select * from Client" 
+    cursor.execute(sql_command)
+    clients=cursor.fetchall
+    return clients
+
+
+###### Post Method #####
+
+
+
+
+######Update Method #####
+
+
+
+#### Delete Method #####
--- menu.sql
+++ menu.sql
@@ -1,17 +1,17 @@
 Create database Order_System;
-Create table Client(
+Create table Clients(
 No_Client int AUTO_INCREMENT primary key, 
 Client_Name VARCHAR (255),
 No_Telephone VARCHAR (255) Unique 
 );
-Create table Order(
+Create table Orders(
 Order_ID int AUTO_INCREMENT primary key, 
-No_Reservation int NULL 
+No_Reservation int NULL ,
 Order_Date datetime Not Null, 
 Order_Type enum("Dine In", "Take Away", "Reserve" ) NOT NULL , 
 No_Table int Not Null, 
 Note VARCHAR(255) Null, 
-Foriegn key (No_Reservation ) References Reservation (No_Reservation ) 
+Foreign key (No_Reservation ) References Reservation (No_Reservation ) 
 ); 
 Create table Product(
 No_Product int AUTO_INCREMENT primary key, 
@@ -26,18 +26,17 @@ Item_ID int AUTO_INCREMENT primary key,
 Order_ID int, 
 No_Product int, 
 Quantity int, 
-Foriegn key (No_Product) References Product(No_Produit), 
-Foriegn key (Order_ID ) References Order(Order_ID) 
+Foreign key (No_Product) References Product(No_Product), 
+Foreign key (Order_ID ) References Orders(Order_ID) 
 ); 
 Create table Payment (
 Payment_ID int AUTO_INCREMENT primary key, 
 Order_ID int, 
 Total_Amount float, 
 Payment_Date datetime, 
-Payment_Method enum("Cash", "MTN Money", "Orange Money" ) NOT NULL 
-Payment_Status
- VARCHAR(255), 
-Foriegn key (Order_ID ) References Order(Order_ID) 
+Payment_Method enum("Cash", "MTN Money", "Orange Money" ) NOT NULL ,
+Payment_Status VARCHAR(255), 
+Foreign key (Order_ID ) References Orders(Order_ID) 
 );
 
 Create table Reservation (
@@ -46,12 +45,13 @@ No_Client int,
 Reservation_Date Date, 
 Reservation_Time Time, 
 No_Person int, 
-Foriegn key (No_Client ) References Client(No_Client),
+Foreign key (No_Client ) References Clients(No_Client)
 );
 
 create table Stock(
 No_Stock int auto_increment primary key,
 No_Product int,
-Quantity_available int
-Foriegn key (No_Product) References Product(No_Product) 
+Quantity_available int,
+Foreign key (No_Product) References Product(No_Product) 
 );
+
